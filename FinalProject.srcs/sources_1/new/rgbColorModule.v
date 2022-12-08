db92a32ad0bedfe6e7f42b5bc30429736f0aa594
S1xZ <punya.gnwdn@gmail.com>
2022-12-09 00:08:06 +0700

    fix cycle clock

--- FinalProject.srcs/sources_1/new/rgbColorModule.v
+++ FinalProject.srcs/sources_1/new/rgbColorModule.v
@@ -22,20 +22,28 @@
 
 module rgbColorModule(input clk,output [11:0]rgb_out);
 
-reg rgb=12'hE00;
-reg [2:0]state;
+    reg rgb=12'hE00;
+    reg [2:0]state;
 
+    wire [2:0] tclk;
+    
+    assign tclk[0]=clk;
+    
+    genvar c;
+    generate for(c=0;c<2;c=c+1) begin
+        clockDiv fDiv(tclk[c+1],tclk[c]);
+    end endgenerate
 
-always@(posedge clk)begin
-if(rgb==12'hE00) state<=0;
-if(rgb==12'hEE0) state<=1;
-if(rgb==12'h0E0) state<=2;
-if(rgb==12'h0EE) state<=3;
-if(rgb==12'h00E) state<=4;
-if(rgb==12'hE0E) state<=5;
-end
+    always@(posedge tclk[1])begin
+        if(rgb==12'hE00) state<=0;
+        if(rgb==12'hEE0) state<=1;
+        if(rgb==12'h0E0) state<=2;
+        if(rgb==12'h0EE) state<=3;
+        if(rgb==12'h00E) state<=4;
+        if(rgb==12'hE0E) state<=5;
+    end
 
-always@(posedge clk)begin
+    always@(posedge tclk[2])begin
     case(state)
         3'd0: rgb<=rgb+12'h010;
         3'd1: rgb<=rgb-12'h100;
@@ -46,6 +54,6 @@ always@(posedge clk)begin
     endcase
 end
 
-assign rgb_out = rgb;
+    assign rgb_out = rgb;
 
 endmodule
